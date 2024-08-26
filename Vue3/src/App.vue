7e121088a6dccf4a8b9ea922df0ecd8bb7cb158d
dugun319 <110073858+dugun319@users.noreply.github.com>
2024-08-26 14:09:49 +0900

    Add files via upload

---
+++ Vue3/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>book.author: {{ Book.author }}</p>
+    <p>book.title: {{ Book.title }}</p>
+    <p>author: {{ author }}</p>
+    <p>title: {{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Vue3/src/backup/02/App_compositionApi.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Reactive Message</h2>
+    <p>{{ reactiveMessage }}</p>
+    <h2>Simple Message</h2>
+    <p>{{ simpleMessage }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup () {
+    console.log('setup()')
+    const reactiveMessage     = ref('Hi reactiveMessage')
+    const addReactiveMessage  = ()=> {
+      reactiveMessage.value   = reactiveMessage.value + '!'
+    } 
+
+    let simpleMessage         = 'Hi simpleMessage'
+    const addSimpleMessage    = ()=> {
+      simpleMessage           = simpleMessage + '!'
+    }
+    return {
+      reactiveMessage,
+      addReactiveMessage,
+      simpleMessage,
+      addSimpleMessage
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
